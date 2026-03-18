5039542fe0ac52cfaf49a2c09e67f284e64a9a78
VireakGaMi <roeunvireak0@gmail.com>
2026-03-18 15:09:05 +0700

    pull

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::$validateKeyPermissions = filter_var(
-            env('PASSPORT_VALIDATE_KEY_PERMISSIONS', false),
-            FILTER_VALIDATE_BOOL
-        );
+        Passport::$validateKeyPermissions = false;
 
         Passport::tokensCan([
             'feed:read' => 'Read the community feed',
